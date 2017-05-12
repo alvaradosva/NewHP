9258661e8ab7a4cc1d0d67991a75d2c748c84b4a
alvaradosva <alvaradosva@gmail.com>
2017-05-12 11:29:32 -0400

    JS edits
    Dropdown links
    Test page / experimental area

--- css/js/bootstrapb.js
+++ css/js/bootstrapb.js
@@ -2422,3 +2422,13 @@ $(document).ready(function() {
     });
     
 });
+
+  // FADE IN STUFF
+var divs = $('.social, .title');
+$(window).scroll(function(){
+   if($(window).scrollTop()<10){
+         divs.stop(true,true).fadeIn("fast");
+   } else {
+         divs.stop(true,true).fadeOut("fast");
+   }
+});
